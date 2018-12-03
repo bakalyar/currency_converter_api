ce86a5d850feea23b14c1190cb18f64702ca7a8d
Andriy Bakalyar <abakalyar@adyax.com>
2018-12-03 13:04:18 +0200

    Provided method getAllCurrencies().

--- src/Plugin/CurrencyConverterApiProvider/FreeCurrencyConverterApi.php
+++ src/Plugin/CurrencyConverterApiProvider/FreeCurrencyConverterApi.php
@@ -3,6 +3,8 @@
 namespace Drupal\currency_converter_api\Plugin\CurrencyConverterApiProvider;
 
 use Drupal\currency_converter_api\CurrencyConverterApiProviderBase;
+use GuzzleHttp\Exception\RequestException;
+use Drupal\Component\Serialization\Json;
 
 /**
  * Provides a provider for Free Currency Converter API.
@@ -14,4 +16,34 @@ use Drupal\currency_converter_api\CurrencyConverterApiProviderBase;
  * )
  */
 class FreeCurrencyConverterApi extends CurrencyConverterApiProviderBase {
+
+  public function getAllCurrencies() {
+    $cache_id = 'free_currency_converter_api:all_currencies';
+    $host = $this->getHost();
+    $url = $host . '/api/v6/currencies';
+
+    $cached = $this->cacheGet($cache_id);
+    if ($cached) {
+      return $cached->data;
+    }
+
+    try {
+      $response = $this->httpClient->request('GET', $url);
+    }
+    catch (RequestException $e) {
+      throw new RequestException("Could not retrieve the currencies from $url", NULL, $e);
+    }
+
+    $response_result = Json::decode($response->getBody()->__toString());
+    $results = !empty($response_result['results']) ? $response_result['results'] : [];
+
+    if (empty($response_result['results'])) {
+      return [];
+    }
+
+    $this->cacheSet($cache_id, $results);
+
+    return $results;
+  }
+
 }
